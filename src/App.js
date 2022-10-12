6e7dce3964ce4c20dd791c878cd8c151cd77c7c6
Sudheer Kumar Prajapat <sudheerkumar8929@gmail.com>
2022-10-12 22:20:51 +0530

    Implemented Context API

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import {TooltipComponent} from "@syncfusion/ej2-react-popups"
 import {Navbar, Footer, Sidebar, ThemeSettings} from './components';
 import {Ecommerce, Orders, Kanban, Area, Bar, Pie, Financial, Calendar ,ColorPicker, ColorMapping, Customers, Editor, Employees, Line, Pyramid, Stacked} from './pages';
 import "./App.css";
+import {useStateContext} from "./contexts/ContextProvider";
 
 const App = () => {
-  const activeMenu = true;
+  const {activeMenu} = useStateContext();
   return (
     <div>
       <Router>
@@ -30,7 +31,7 @@ const App = () => {
           )}
           <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${(activeMenu)?("md:ml-72"):("flex-2")}`}>
             <div className="fixed md-static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-              Navbar
+              <Navbar/>
             </div>
           </div>
           <div>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import {ContextProvider} from './contexts/ContextProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <ContextProvider>
+      <App/>
+    </ContextProvider>
   </React.StrictMode>, document.getElementById('root')
 );
