6406f6397d34fab4f8e5095d54efe7ba4dee2218
Sudheer Kumar Prajapat <sudheerkumar8929@gmail.com>
2022-10-12 21:31:25 +0530

    Dashboard Designing Part Almost Completed

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ const App = () => {
           </div>
           {(activeMenu)?(
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-              Sidebar 
+              <Sidebar/>
             </div>):(
             <div className="w-0 dark:bg:secondary-dark-bg">
-              Sidebar w-0
+              <Sidebar/>
             </div>
           )}
           <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${(activeMenu)?("md:ml-72"):("flex-2")}`}>
